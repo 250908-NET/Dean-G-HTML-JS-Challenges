8b1beebbf2bfc92a512ad838715780d98e007028
Dean <deanscottg@gmail.com>
2025-10-08 10:24:45 -0400

    Finished Challenge9

--- Intro_to_JavaScript/Challenge8/script.js
+++ Intro_to_JavaScript/Challenge8/script.js
@@ -1,1 +1,14 @@
-console.log("Hello from the external script!");
+function sayHello() {
+  alert("Hello from the external file!");
+}
+
+// Connect the function to the button
+let helloBtn = document.getElementById("helloBtn");
+helloBtn.addEventListener("click", sayHello);
+
+function sayGoodbye() {
+  alert("Peace out!");
+}
+
+let goodByeBtn = document.getElementById("goodByeBtn");
+goodByeBtn.addEventListener("click", sayGoodbye);
